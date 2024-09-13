cc66799780f69568eee4652e4bf23962f81c3408
Lay <1921450767@qq.com>
2024-09-13 10:41:11 +0800

    trytry

---
+++ temp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         CC98查询粉丝的变动
+// @namespace    https://www.cc98.org/user/id/698075
+// @version      2024-09-13
+// @description  to find out how my fans change
+// @author       Lay
+// @match        https://www.cc98.org/usercenter/myfans
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('hello laylay');
+
+    // Your code here...
+})();
